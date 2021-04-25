c522b12f9e2937e12b132db2d0d5108f001af1a0
pranav661 <37332556+pranav661@users.noreply.github.com>
2021-04-25 22:58:37 +0530

    Update pre-requisites.sh

--- pre-requisites.sh
+++ pre-requisites.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Installing Pre-Requisites Software"
-sudo apt-get install python3-pip
-sudo apt-get install jq
+sudo apt-get install python3-pip -y
+sudo apt-get install jq -y
 pip3 install yq
 chmod u+x updateYamlConfig.sh
