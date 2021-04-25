4b4df5bcbba0fb239c1473d6571b888967038b92
pranav661 <37332556+pranav661@users.noreply.github.com>
2021-04-25 23:20:45 +0530

    Update pre-requisites.sh

--- pre-requisites.sh
+++ pre-requisites.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
 echo "Installing Pre-Requisites Software"
-sudo apt-get install git -y
 sudo apt-get install python3-pip -y
 sudo apt-get install jq -y
 pip3 install yq
 
-echo "Clonning Repository"
-git clone https://github.com/pranav661/DevOpsTask.git
-
-#Give execute permission to script
-cd DevOpsTask
-chmod u+x updateYamlConfig.sh
